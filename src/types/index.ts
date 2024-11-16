8aedabe1dc3e3aa93653d02b168d6ff8318a9397
AlexEP11 <aleeps2911@gmail.com>
2024-11-16 11:01:38 -0600

    feat: add Player type

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export type Player = {
+    equipo: string;
+    carnet: string;
+    curp: string;
+    nombre: string;
+    apellido_paterno: string;
+    apellido_materno: string;
+    categoria: string;
+    fecha_nacimiento: Date;
+    años_registro: number;
+    foto: File;
+};
