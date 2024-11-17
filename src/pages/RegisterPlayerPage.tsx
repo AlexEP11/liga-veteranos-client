d6d835f0ab12d603fbcdbcb81ff360f191779f16
AlexEP11 <aleeps2911@gmail.com>
2024-11-17 14:54:54 -0600

    feat(formPlayer): add animation

--- src/pages/RegisterPlayerPage.tsx
+++ src/pages/RegisterPlayerPage.tsx
@@ -1,15 +1,27 @@
+import { Grow } from "@mui/material";
+import { useEffect, useState } from "react";
 import CardPlayer from "../components/CardPlayer";
 import FormPlayer from "../components/FormPlayer";
 import "@fontsource/roboto/400.css";
 export default function RegisterPlayerPage() {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
     return (
         <>
-            <div className="flex flex-col gap-8 lg:flex-row md:gap-32 items-center px-4 md:px-0 ">
-                <FormPlayer />
-                <div className="hidden md:block">
-                    <CardPlayer />
-                </div>
-            </div>
+            {mounted && (
+                <Grow in={true} timeout={1000} style={{ transformOrigin: "0 0 0" }}>
+                    <div className="flex flex-col gap-8 lg:flex-row md:gap-32 items-center px-4 md:px-0">
+                        <FormPlayer />
+                        <div className="hidden md:block">
+                            <CardPlayer />
+                        </div>
+                    </div>
+                </Grow>
+            )}
         </>
     );
 }
